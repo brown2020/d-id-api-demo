8bbfceb8e2b041dd1a7e81284e258e1539027a9c
Steve Brown <brown2020@gmail.com>
2024-08-16 09:47:11 -0700

    Footer component updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ type Props = {};
 export default function Footer({}: Props) {
   return (
     <div className="flex items-center justify-center mx-auto h-14 px-4 py-2">
-      Footer
+      D-ID Api Demo
     </div>
   );
 }
