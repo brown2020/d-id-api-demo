2d2111f4e5f3315f9a1357e8bb86276fa21a2a53
Steve Brown <brown2020@gmail.com>
2024-08-16 17:41:53 -0700

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/profile(.*)",
-  "/generate(.*)",
   "/avatars(.*)",
+  "/generate(.*)",
+  "/payment-attempt(.*)",
+  "/payment-success(.*)",
+  "/profile(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
